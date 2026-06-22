5d7a55533ccd840494786d3b7b3461544b71b148
Olha Filatova <olyafilatov@gmail.com>
2026-06-22 18:34:34 +0300

    simplest under development page

---
+++ src/i18n/app.ts
@@ -0,0 +1,9 @@
+export const texts: Record<string, {
+  "ua": string;
+  "en": string;
+}> = {
+  "welcome": {
+    "ua": "Вітаю! Я - Оля Філатова. Ця сторінка наразі знаходиться у стадії розробки.",
+    "en": "Welcome! I'm Olha Filatova. This page is currently under development."
+  }
+} as const;
---
+++ src/i18n/config.ts
@@ -0,0 +1,31 @@
+export const languages = ['ua', 'en'] as const;
+
+export type Language = typeof languages[number];
+
+function isLanguage(lang: string | null): lang is Language {
+  return !!lang && languages.includes(lang as any);
+}
+
+function parseLangCache(): Language {
+  const storedLanguage = localStorage.getItem('lang');
+  if (isLanguage(storedLanguage)) {
+    return storedLanguage;
+  }
+  return 'ua';
+}
+
+const languageControls = (function(){
+  let currentLanguage: Language = parseLangCache();
+  return {
+    getCurrentLanguage: function getCurrentLanguage() {
+      return currentLanguage;
+    },
+    changeCurrentLanguage: function changeCurrentLanguage(lang: Language) {
+      currentLanguage = lang;
+      localStorage.setItem('lang', currentLanguage);
+    }
+  }
+})();
+
+export const getCurrentLanguage = languageControls.getCurrentLanguage;
+export const changeCurrentLanguage = languageControls.changeCurrentLanguage;
