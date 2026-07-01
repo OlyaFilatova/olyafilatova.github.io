9d1815504ec3ce5c4f0db973d54c8eadb3b46977
Olha Filatova <olyafilatov@gmail.com>
2026-07-01 19:20:57 +0300

    add retry logic

--- src/components/KnowledgeSourceLoader.tsx
+++ src/components/KnowledgeSourceLoader.tsx
@@ -5,10 +5,19 @@ import KnowledgeSourceItem from './KnowledgeSourceItem';
 
 function KnowledgeSourceLoader({ filePath }: {filePath: string}) {
   const [source, setSource] = useState<KnowledgeSource | undefined>()
-  useEffect(() => {
+
+  function loadData(filePath: string, retriesCount = 0, maxRetriesCount = 5) {
     fetch(filePath).then(async resp => {
-      setSource(KnowledgeSource.parse(await resp.json()))
+      setSource(KnowledgeSource.parse(await resp.json()));
+    }).catch(async () => {
+      if (retriesCount < maxRetriesCount) {
+        setTimeout(() => loadData(filePath, retriesCount + 1), 100);
+      }
     })
+  }
+
+  useEffect(() => {
+    loadData(filePath);
   }, []);
 
   return (
