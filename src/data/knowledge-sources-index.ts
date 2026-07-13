e899e5a150384d1e609a823c76bba0eb51e1acad
Olha Filatova <olyafilatov@gmail.com>
2026-07-13 13:19:10 +0300

    assemble planned sources

--- src/data/knowledge-sources-index.ts
+++ src/data/knowledge-sources-index.ts
@@ -1,18 +1,18 @@
 
 import type { ThoughtsIndex } from '../schemas/knowledge-source.ts';
 
-export const bustCache = 13;
+export const bustCache = 14;
 
 export const index: ThoughtsIndex = {
-  "count": 25,
+  "count": 28,
   "categories": {
     "Non": [
       0,
-      9,
-      10,
-      11,
-      12,
-      13,
+      1,
+      2,
+      3,
+      4,
+      5,
       14,
       15,
       16,
@@ -23,126 +23,55 @@ export const index: ThoughtsIndex = {
       21,
       22,
       23,
-      24
+      24,
+      25,
+      26,
+      27
     ],
     "Python": [
-      1,
-      4,
-      5,
       6,
-      7
+      9,
+      10,
+      11,
+      12
     ],
     "Data analysis": [
-      1
+      6
     ],
     "NumPy": [
-      1
+      6
     ],
     "Pandas": [
-      1
+      6
     ],
     "matplotlib": [
-      1
+      6
     ],
     "JS": [
-      2,
-      3,
-      8
+      7,
+      8,
+      13
     ],
     "Functional programming": [
-      2,
-      3
+      7,
+      8
     ],
     "Django": [
-      4,
-      5,
-      6,
-      7
+      9,
+      10,
+      11,
+      12
     ]
   },
   "kind": {
     "book": [
       0,
-      1,
-      2,
-      3,
-      8,
-      9,
-      10,
-      11,
-      12,
-      13,
-      14,
-      15,
-      16,
-      17,
-      18,
-      19,
-      20,
-      21,
-      22,
-      23,
-      24
-    ],
-    "documentation": [
-      4,
-      5,
-      6,
-      7
-    ]
-  },
-  "access": {
-    "paid": [
-      0,
-      9,
-      10,
-      15,
-      23
-    ],
-    "free": [
-      1,
-      2,
       3,
       4,
       5,
       6,
       7,
       8,
-      11,
-      12,
-      13,
-      14,
-      16,
-      17,
-      18,
-      19,
-      20,
-      21,
-      22,
-      24
-    ]
-  },
-  "status": {
-    "first-read": [
-      0,
-      2,
-      3,
-      4,
-      5,
-      6,
-      7
-    ],
-    "in-progress-first-read": [
-      1
-    ],
-    "paused": [
-      8
-    ],
-    "planned": [
-      9,
-      10,
-      11,
-      12,
       13,
       14,
       15,
@@ -154,7 +83,90 @@ export const index: ThoughtsIndex = {
       21,
       22,
       23,
-      24
+      24,
+      25,
+      26,
+      27
+    ],
+    "documentation": [
+      1,
+      2,
+      9,
+      10,
+      11,
+      12
+    ]
+  },
+  "access": {
+    "paid": [
+      0,
+      3,
+      14,
+      15,
+      18,
+      26
+    ],
+    "free": [
+      1,
+      2,
+      4,
+      5,
+      6,
+      7,
+      8,
+      9,
+      10,
+      11,
+      12,
+      13,
+      16,
+      17,
+      19,
+      20,
+      21,
+      22,
+      23,
+      24,
+      25,
+      27
+    ]
+  },
+  "status": {
+    "first-read": [
+      0,
+      7,
+      8,
+      9,
+      10,
+      11,
+      12
+    ],
+    "planned": [
+      1,
+      2,
+      3,
+      4,
+      5,
+      14,
+      15,
+      16,
+      17,
+      18,
+      19,
+      20,
+      21,
+      22,
+      23,
+      24,
+      25,
+      26,
+      27
+    ],
+    "in-progress-first-read": [
+      6
+    ],
+    "paused": [
+      13
     ]
   }
 }
