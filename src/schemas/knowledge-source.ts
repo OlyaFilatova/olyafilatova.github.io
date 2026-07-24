737c6020e54cf32a2cc62c4e83c489080028b79f
Olha Filatova <olyafilatov@gmail.com>
2026-07-24 16:13:18 +0300

    reformat knowledge sources files

--- src/schemas/knowledge-source.ts
+++ src/schemas/knowledge-source.ts
@@ -8,15 +8,20 @@ const localizedString = zod.object({
 const Status = zod.enum(['planned', 'in-progress-first-read', 'first-read', 'in-progress-thorough-read', 'read', 'paused']);
 const Access = zod.enum(['free', 'paid']);
 
-export const KnowledgeSource = zod.object({
+export const KnowledgeSourceMeta = zod.object({
   "kind": zod.enum(["book", "documentation", "paper", "web-page", "specification"]),
   "status": Status,
   "title": localizedString,
   "access": Access,
   "link": zod.string().min(1),
-  "thoughts": zod.optional(zod.array(localizedString)),
   "categories": zod.optional(zod.array(zod.string())),
   "date": zod.optional(zod.string().pipe(zod.coerce.date()))
+});
+
+export const KnowledgeSourceContent = zod.array(localizedString);
+
+export const KnowledgeSource = KnowledgeSourceMeta.extend({
+  "thoughts": zod.optional(zod.array(localizedString)),
 });
 
 export type Status = zod.infer<typeof Status>;
