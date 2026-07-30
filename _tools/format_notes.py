d77a8778e747faf867efddb363caecae0068fe14
Olha Filatova <olyafilatov@gmail.com>
2026-07-30 11:09:54 +0300

    translate notes tool

---
+++ _tools/format_notes.py
@@ -0,0 +1,87 @@
+# used to format and translate notes created using chrome extension https://github.com/OlyaFilatova/page-notes
+# IMPORTANT: uses service from https://github.com/OlyaFilatova/ai_projects/tree/main/03_translate
+import json
+from pathlib import Path
+from typing import TypedDict
+import requests
+
+class Note(TypedDict):
+  text: str
+
+class Mapping(TypedDict):
+  url: str
+  notes: list[Note]
+
+
+def get_script_dir():
+  return Path(__file__).resolve().parent
+
+def get_notes_dir(root_dir: Path):
+  return root_dir / 'notes'
+
+def get_files(dir: Path):
+  return [f for f in dir.iterdir() if f.is_file() and f"{f}".endswith(".json")]
+
+def read_files(files: list[Path]):
+  def get_file_pages(file: Path):
+    with open(file, 'r') as f:
+      content = json.load(f)
+      return content["mappingsByUrl"]
+  return [get_file_pages(file) for file in files]
+
+def format_pages(mappingsByUrlList: list[dict[str, Mapping]]):
+  return [{
+      "url": value["url"],
+      "notes": [note["text"] for note in value["notes"]]
+    } for mapping in mappingsByUrlList for value in mapping.values()]
+
+def create_result_path(root_dir: Path, counter: int):
+  return root_dir / 'formatted' / f'{counter}.json'
+
+def translate_page(page, counter):
+  url = "http://localhost:8000/translate"
+  notes = [*reversed(page["notes"])]
+  payload = {
+    "texts": notes,
+    "direction": "en_to_uk"
+  }
+
+  response = requests.post(url, json=payload)
+
+  translated = response.json()["translations"]
+
+  thoughts = [{
+    "uk": uk,
+    "en": en
+  } for en, uk in zip(notes, translated)]
+
+  return counter, {
+    "kind": "unknown",
+    "status": "first-read",
+    "title": {
+      "uk": "Unknown",
+      "en": "Unknown"
+    },
+    "access": "free",
+    "link": page["url"],
+    "thoughts": thoughts
+  }
+
+def store_result(result_path: Path, res):
+  with open(result_path, 'w') as f:
+    json.dump(res, f, ensure_ascii=False, indent=2)
+
+def translate_pages(pages, root_dir):
+  for idx, page in enumerate(pages):
+    store_result(create_result_path(root_dir, idx), translate_page(page, idx))
+
+script_dir = get_script_dir()
+
+translate_pages(
+  format_pages(
+    read_files(
+      get_files(
+        get_notes_dir(
+          script_dir)))),
+  script_dir
+)
