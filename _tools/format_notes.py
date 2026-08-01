afb7bfb9868141ee083e01453a1139f4b3303aa3
Olha Filatova <olyafilatov@gmail.com>
2026-08-01 17:56:37 +0300

    refactor tool code

--- _tools/format_notes.py
+++ _tools/format_notes.py
@@ -1,6 +1,7 @@
 # used to format and translate notes created using chrome extension https://github.com/OlyaFilatova/page-notes
 # IMPORTANT: uses service from https://github.com/OlyaFilatova/ai_projects/tree/main/03_translate
 import json
+import os
 from pathlib import Path
 from typing import TypedDict
 import requests
@@ -22,72 +23,71 @@ def get_notes_dir(root_dir: Path):
 def get_files(dir: Path):
   return [f for f in dir.iterdir() if f.is_file() and f"{f}".endswith(".json")]
 
-def read_files(files: list[Path]):
-  def get_file_pages(file: Path):
-    with open(file, 'r') as f:
-      content = json.load(f)
-      return content["mappingsByUrl"]
-  return [get_file_pages(file) for file in files]
+def read_file(file: Path) -> dict[str, Mapping]:
+  with open(file, 'r') as f:
+    content = json.load(f)
+    return content["mappingsByUrl"]
 
-def format_pages(mappingsByUrlList: list[dict[str, Mapping]]):
+def format_file(mappingsByUrl: dict[str, Mapping]):
   return [{
       "url": value["url"],
       "notes": [note["text"] for note in value["notes"]]
-    } for mapping in mappingsByUrlList for value in mapping.values()]
+    } for value in mappingsByUrl.values()]
 
-def translate_pages(pages):
-  def translate_page(page, counter):
-    url = "http://localhost:8000/translate"
-    notes = [*reversed(page["notes"])]
-    payload = {
-      "texts": notes,
-      "direction": "en_to_uk"
-    }
+def translate_page(page):
+  url = "http://localhost:8000/translate"
+  notes = [*reversed(page["notes"])]
+  payload = {
+    "texts": notes,
+    "direction": "en_to_uk"
+  }
 
-    response = requests.post(url, json=payload)
+  response = requests.post(url, json=payload)
 
-    translated = response.json()["translations"]
+  translated = response.json()["translations"]
 
-    thoughts = [{
-      "uk": uk,
-      "en": en
-    } for en, uk in zip(notes, translated)]
+  return zip(notes, translated)
 
-    return counter, ({
-      "kind": "unknown",
-      "status": "first-read",
-      "title": {
-        "uk": "Unknown",
-        "en": "Unknown"
-      },
-      "access": "free",
-      "categories": [],
-      "link": page["url"]
-    }, thoughts)
+def format_page(page, notes):
+  thoughts = [{
+    "uk": uk,
+    "en": en
+  } for en, uk in notes]
 
-  return [(idx, translate_page(page, idx)) for idx, page in enumerate(pages)]
-    
-def store_pages(pages, root_dir):
-  def create_result_path(root_dir: Path, counter: int):
-    return root_dir / 'formatted' / f'{counter}'
+  return ({
+    "kind": "unknown",
+    "status": "first-read",
+    "title": {
+      "uk": "Unknown",
+      "en": "Unknown"
+    },
+    "access": "free",
+    "categories": [],
+    "link": page["url"]
+  }, thoughts)
 
-  def store_result(result_path: Path, meta, content):
-    with open(result_path / 'meta.json', 'w') as f:
-      json.dump(meta, f, ensure_ascii=False, indent=2)
+def create_result_path(root_dir: Path, dir_name: str):
+  return root_dir / 'formatted' / f'{dir_name}'
 
-    with open(result_path / 'content.json', 'w') as f:
-      json.dump(content, f, ensure_ascii=False, indent=2)
+def store_page(result_path: Path, page):
+  meta, content = page
+  os.makedirs(result_path, exist_ok=True)
+  with open(result_path / 'meta.json', 'w') as f:
+    json.dump(meta, f, ensure_ascii=False, indent=2)
 
-  [store_result(create_result_path(root_dir, idx), meta, content) for idx, (meta, content) in pages]
+  with open(result_path / 'content.json', 'w') as f:
+    json.dump(content, f, ensure_ascii=False, indent=2)
 
 script_dir = get_script_dir()
 
-store_pages(
-  translate_pages(
-    format_pages(
-      read_files(
-        get_files(
-          get_notes_dir(
-            script_dir
-          ))))),
-  script_dir)
+pages = (
+  (file_idx, page_idx, page)
+  for file_idx, file in enumerate(get_files(get_notes_dir(script_dir)))
+  for page_idx, page in enumerate(format_file(read_file(file)))
+)
+
+for file_idx, page_idx, page in pages:
+  store_page(
+    create_result_path(script_dir, f"{file_idx}-{page_idx}"),
+    format_page(page, translate_page(page)),
+  )
